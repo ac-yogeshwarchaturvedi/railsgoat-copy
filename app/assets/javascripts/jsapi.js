eec20c915ecbcfe48e55b279c29fe265ebcb9db7
Yogeshwar Chaturvedi <148308873+ac-yogeshwarchaturvedi@users.noreply.github.com>
2024-05-13 12:54:06 +0530

    Update jsapi.js

--- app/assets/javascripts/jsapi.js
+++ app/assets/javascripts/jsapi.js
@@ -5,7 +5,6 @@ if (!window['google']['loader']) {
 window['google']['loader'] = {};
 google.loader.ServiceBase = 'https://www.google.com/uds';
 google.loader.GoogleApisBase = 'https://ajax.googleapis.com/ajax';
-google.loader.KeyVerified = true;
 google.loader.LoadFailure = false;
 google.loader.Secure = true;
 google.loader.GoogleLocale = 'www.google.com';
